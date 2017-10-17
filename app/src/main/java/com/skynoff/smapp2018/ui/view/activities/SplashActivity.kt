cd23369bb439c6091358d184cdbe2f783081f5f6
smithnoff <ingcesarsmith@gmail.com>
2017-10-17 11:15:33 -0500

    Initial commit

---
+++ app/src/main/java/com/skynoff/smapp2018/ui/view/activities/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.skynoff.smapp2018.ui.view.activities
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.skynoff.smapp2018.R
+
+class SplashActivity : AppCompatActivity() {
+    internal var SPLASH_TIME_OUT = 2000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler().postDelayed(
+                {
+                    startActivity(Intent(this, LoginActivity::class.java))
+                    finish()
+                }, SPLASH_TIME_OUT.toLong())
+    }
+}
